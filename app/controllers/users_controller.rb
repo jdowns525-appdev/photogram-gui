ef5b95b0f4e50006b049fd7c7e1a4a8d5a92dc53
jdowns525 <downsjosseph@gmail.com>
2023-03-09 18:21:52 +0000

    commit 3. details for /user/[USER ID]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    render({ :template => "user_templates/show.html.erb"})
+      # Parameters: {"path_username" =>"anisa"}
+      url_username = params.fetch("path_username")
+
+      matching_usernames = User.where({ :username => url_username })
+
+      @the_user = matching_usernames.at(0)
+
+      #if @the_user == nil
+      #  redirect_to("/404")
+      #else  
+  render({ :template => "user_templates/show.html.erb"})
+    #end
   end
 end
