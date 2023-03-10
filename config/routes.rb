cc3e7e6f38d901f81502ebce18b3de9643084760
jdowns525 <downsjosseph@gmail.com>
2023-03-10 18:02:11 +0000

    added /photo_ID and create commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ get("/photos", { :controller => "photos", :action => "index" })
 get("/photos/:path_id", { :controller => "photos", :action => "show"})
 
 get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii"})
+
+get("/insert_photo", { :controller => "photos", :action => "create" })
 end
