db04259e5a2397b409b7f78ea150d16a9c77df8d
jdowns525 <downsjosseph@gmail.com>
2023-03-10 16:31:12 +0000

    added /delete_photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -19,4 +19,19 @@ class PhotosController < ApplicationController
 
     render({ :template => "photo_templates/show.html.erb"})
   end
+
+  def baii
+
+    #Parameters: {"toast_id"=>"785"}
+
+    the_id = params.fetch("toast_id")
+
+    matching_photos = Photo.where({ :id => the_id })
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    render({ :template => "photo_templates/baii.html.erb"})
+  end
 end
